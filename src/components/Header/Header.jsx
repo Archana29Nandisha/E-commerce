dbdaacb242c9215c8aa96de146a2264cb2e5c00f
Archana29Nandisha <125017946+Archana29Nandisha@users.noreply.github.com>
2023-06-25 10:41:02 +0530

    Using material UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,27 @@
-import menu from '../../images/menu-navigation-grid-1528-svgrepo-com.svg'
-import cart from '../../images/cart-4-svgrepo-com.svg'
-import search from '../../images/search-svgrepo-com.svg'
+
+import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+import { IconButton } from '@mui/material';
+
+
 const Header = () => {
     return(
-        <header className=' fixed top-0 left-0 z-[100] w-full p-5 bg-my-background flex items-center justify-between drop-shadow-header-shadow'>
+        <header className=' fixed top-0 left-0 z-[100] w-full p-5 bg-my-background flex items-center justify-between drop-shadow-header-shadow py-5 px-2'>
             {/* header left */}
             <div className='flex items-center'>
-                <img src={menu} alt='menu' className='w-6 h-6 object-contain cursor-pointer'/>
+                <IconButton><MenuIcon/></IconButton>
                 <h1 className=' ml-2 text-sm font-bold'>Archi
                 <span className=' text-neutral-600'>29</span>
                 </h1>
                 <div className=' overflow-hidden ml-2 flex items-center rounded-full bg-slate-500 '>
                     <input type= 'text' placeholder="Search" className=' ml-1 outline-none p-2 font-semibold text-sm w-[200px] bg-inherit'/>
-                    <img src={search} alt='search'className='w-6 h-6 object-contain cursor-pointer mr-2'/>
+                    <IconButton><SearchIcon className=' text-gray-700'/></IconButton>
                 </div>
-                <h1>bbb</h1>
+            
             </div>
             {/* header right */}
-            <img src={cart} alt='cart' className='w-6 h-6 object-contain cursor-pointer'/>
+            <IconButton><ShoppingCartIcon/></IconButton>
         </header>
     )
 }
